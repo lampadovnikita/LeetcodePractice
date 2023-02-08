449659fc8ecc0e6bd6c3092f21e6ee1a209cc8dc
Lampadov Nikita <lampadovnikita@gmail.com>
2023-02-09 03:50:07 +0700

    Create 136.singlenumber.go

---
+++ solutions/136.singlenumber.go
@@ -0,0 +1,28 @@
+// 136. Single Number
+//
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+//
+// Example 1:
+// Input: nums = [2,2,1]
+// Output: 1
+//
+// Example 2:
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+//
+// Example 3:
+// Input: nums = [1]
+// Output: 1
+
+package main
+
+func singleNumber(nums []int) int {
+	var res int
+	// xor with similar number always results to zero
+	for _, n := range nums {
+		res ^= n
+	}
+
+	return res
+}
